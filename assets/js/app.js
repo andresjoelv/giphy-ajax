15f49efb88b7df04e0b4eaca15a63fd16033e6eb
Nejo <andresjoelv@gmail.com>
2018-10-30 14:54:25 -0400

    working on html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,24 @@
 $(function() {
     // Initial array of movies
-    var topics = ["COD", "fifa18", "rainbow six siege", "outlast ps4"];
+    var games = ["COD", "fifa18", "rainbow six siege", "outlast ps4"];
 
     // api call
     const api_key = "4i2cKQvU5RTeTcXWvIdf172GA46qQCaV";
     let url = "https://api.giphy.com/v1/gifs/search"; //host+path
 
-    var isPLayingGif = false;
+    //var isPLayingGif = false;
     var still = "";
     var lastbtn = "";
     var gifUrls = [];
+    var gifUrlsStill = [];
 
+    /*
+    * display buttons
+    */
     function renderButtons(){
         $("#btns-view").empty();
 
-        topics.forEach( topic => {
+        games.forEach( topic => {
             var a = $("<button>")
             
             a.addClass("game");
@@ -25,19 +29,27 @@ $(function() {
     }
 
     /*
-    *   TO-DO
+    *   add button
     */
-    // $("#add-game").on("click", function(e){
-    //     e.preventDefault();
+    $("#add-game").on("click", function(e){
+        e.preventDefault();
 
-    //     var game = $("#game-input").val().trim();
+        var game = $("#game-input").val().trim();
 
-    // });
+        games.push(game);
+
+        renderButtons();
+
+    });
 
     /*
-    *   TO-DO
+    *   display gifs onto screen
     */
     function displayGifs(){
+        // reset arrays
+        gifUrls = [];
+        gifUrlsStill = [];
+
         var game = $(this).attr("data-name");
 
         url = "https://api.giphy.com/v1/gifs/search";
@@ -61,36 +73,43 @@ $(function() {
 
                 var gifImage = $("<img>");
                 gifImage.attr("src", response.data[i].images.fixed_height_still.url);
-                gifImage.attr("data-gif", response.data[i].images.fixed_height.url)
-                //gifUrls.push(response.data[i].images.fixed_height.url);
+                
+                /* for gif click */
+                gifUrls.push(response.data[i].images.fixed_height.url);
+                gifUrlsStill.push(response.data[i].images.fixed_height_still.url);
+                /* * */
+                
                 gifImage.attr("data-gif-id", i)
+                gifImage.attr("data-playing", "static");
+
                 gifImage.addClass("play");
 
-                gifDiv.prepend(p);
-                gifDiv.prepend(gifImage);
+                gifDiv.append(p);
+                gifDiv.append(gifImage);
 
+                
                 $("#giphy-list").prepend(gifDiv);
-
-                isPLayingGif = false;
             }
         });
 
 
     }
 
+    /*
+    * Image manipulation to play Gifs
+    */
     function playGif(){
         // if not playing then play gif
-        if(!isPLayingGif){
-            console.log("play gif");
-            isPLayingGif = true;
-            still = $(this).attr("src");
-            var gif = $(this).attr("data-gif");
-            $(this).attr("src", gif);
+        console.log($(this).attr("data-playing"));
+        if($(this).attr("data-playing") === "static"){
+            $(this).attr("data-playing", "playing");
+            var index = $(this).attr("data-gif-id");
+            $(this).attr("src", gifUrls[parseInt(index)]);
         }
         else {
-            isPLayingGif = false;
-            $(this).attr("src", still);
-            console.log("stop gif");
+            $(this).attr("data-playing", "static");
+            var index = $(this).attr("data-gif-id");
+            $(this).attr("src", gifUrlsStill[parseInt(index)]);
         }
         
     }
